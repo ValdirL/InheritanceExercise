972e10914543ac479d12129fc4d535a1ac1c2b5a
Valdir Lima <valdirlima508@gmail.com>
2022-02-23 12:51:31 -0500

    initial commit

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    internal class Animal
+    {
+        public string Name { get; set; }   
+        public int NumberOfLegs { get; set; }
+        public string FoodSource { get; set; } 
+        public bool IsEndangered { get; set; } 
+
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -20,8 +20,22 @@ namespace Inheritance
             // give this class 4 members that are specific to Reptile
             // Set this class to inherit from your Animal Class
 
+            Bird instance = new Bird();
+            instance.BirdName = "Peacock";
+            instance.CanFly = true;
+            instance.Color = "Multicolor";
+            instance.LaysEggs = false;
 
+            Console.WriteLine($"A {instance.BirdName} is one of the most beautiful bird species. Its tends to be {instance.Color} and it is {instance.CanFly} that it can fly but is {instance.LaysEggs} that it lays eggs.");
 
+            Reptile instance2 = new Reptile();
+            instance2.ReptileName = "Lizard";
+            instance2.CanCamo = true;
+            instance2.IsColdBlooded = true;
+            instance2.IsFriendly = false;
+
+            Console.WriteLine($"I love {instance2.ReptileName}s!! They have the coolest characteristics. I'll admit, its {instance2.IsFriendly} that they are always friendly but its {instance2.CanCamo} that they can camoflauge and {instance2.IsColdBlooded} that the are cold blooded animals");
+         
 
             /*Create an object of your Bird class
              *  give values to your members using the object of your Bird class
